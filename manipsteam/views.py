e234536281193a891a369abfda99805b251057fe
ZaharichS <zhlzny.ahkaz@gmail.com>
2024-12-29 00:05:19 +0300

    Fix main files

--- manipsteam/views.py
+++ manipsteam/views.py
@@ -5,4 +5,4 @@ def home(request):
     # Sort by playtime_hours
     sorted_games = dict(sorted(my_games_and_user_scores.items(), key=lambda item: item[1]['playtime_hours'], reverse=True))
 
-    return render(request, 'manipsteam/home.html', {'response': sorted_games, 'errors': errors})
+    return render(request, 'manipsteam/home.html', {'response': sorted_games})
