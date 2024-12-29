8e13c56561f5fda2351ce28d248802dfcd24fc56
ZaharichS <zhlzny.ahkaz@gmail.com>
2024-12-29 14:46:07 +0300

    Change main logic for get all user games

--- manipsteam/games.py
+++ manipsteam/games.py
@@ -3,6 +3,8 @@ import os
 
 api = 'https://api.steampowered.com/ISteamApps/GetAppList/v2/'
 id = os.getenv('id_key')
+api_key = os.getenv('api_key')
+
 res = requests.get(url=api)
 dict = res.json()['applist']['apps']
 data_clean = {}
@@ -17,29 +19,14 @@ def get_score(id):
     score = data['total_positive']/data['total_reviews']
     return score
 
-user_url = f'http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key={os.getenv('api_key')}&steamid={id}'
+user_url = f'https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/?key={api_key}&steamid={id}&include_appinfo=true&include_extended_appinfo=true&include_played_free_games=true'
 user_game_data = requests.get(url=user_url).json()
 
 games_owned = []
+games_count = user_game_data['response']['game_count']
 for result in user_game_data['response']['games']:
     games_owned.append({
         "appid": result['appid'],
-        "playtime_forever": result['playtime_forever']
+        "name": result['name'],
+        "playtime_forever": round(result['playtime_forever'] / 60)
     })
-    
-my_games_and_user_scores = {}
-for game in games_owned:
-    try:
-        game_name = get_name(game['appid'])
-        playtime_hours = game['playtime_forever'] / 60  # For user-freindly hours view
-        my_games_and_user_scores[game_name] = {
-            "appid": game['appid'],
-            "playtime_hours": round(playtime_hours, 2),  # Rounding to two digits
-        }
-    except Exception as e:
-        print(f"Error fetching game info for {game['appid']}: {e}")
-        playtime_hours = game['playtime_forever'] / 60
-        my_games_and_user_scores[game['appid']] = {
-            "appid": 'Error: not find game name',
-            "playtime_hours": round(playtime_hours, 2),
-        }
--- manipsteam/views.py
+++ manipsteam/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-from .games import my_games_and_user_scores
+# from .games import my_games_and_user_scores, games_count
+from .games import games_owned, games_count
 
 def home(request):
     # Sort by playtime_hours
-    sorted_games = dict(sorted(my_games_and_user_scores.items(), key=lambda item: item[1]['playtime_hours'], reverse=True))
-
-    return render(request, 'manipsteam/home.html', {'response': sorted_games})
+    games_owned.sort(key=lambda item: item['playtime_forever'], reverse=True)
+    return render(request, 'manipsteam/home.html', {'games': games_owned, 'games_count': games_count})
